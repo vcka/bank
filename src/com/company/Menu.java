29e02fec1950a9e26b7d5694881a1ae6447a8769
vcka <vcka@ministras.lt>
2018-11-14 11:08:21 +0200

    Validation

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -12,12 +12,44 @@ public class Menu {
         Scanner scn = new Scanner(System.in);
         System.out.println("Iveskite varda");
         String name = scn.next();
-        System.out.println("Iveskite emaila");
-        String email = scn.next();
-        System.out.println("Iveskite balansa");
-        double balance = scn.nextDouble();
-        System.out.println("Iveskite kredito limita");
-        int creditLimit = scn.nextInt();
+
+        String email;
+        do {
+            System.out.println("Iveskite emaila");
+            email = scn.next();
+            if (email.contains("@")) break;
+            else System.out.println("Neteisingas Email");
+        }while(true);
+
+        double balance;
+        do {
+            System.out.println("Iveskite balansa");
+            String input = scn.next();
+            try {
+                balance = Double.parseDouble(input);
+                break;
+                // number is good.
+
+            } catch (NumberFormatException e) {
+                System.out.println("Neteisingas balansas");
+            }
+        }while (true);
+
+        int creditLimit;
+        do {
+            System.out.println("Iveskite kredito limita");
+            String input = scn.next();
+            try {
+                creditLimit = Integer.parseInt(input);
+                break;
+                //number is good.
+
+            } catch (NumberFormatException e) {
+                System.out.println("Neteisingas kredito limitas");
+            }
+        }while (true);
+
+
 
         if(creditLimit > 0) {
             Account user1 = new Account(name,email,balance, creditLimit);
@@ -31,6 +63,5 @@ public class Menu {
             Account user1 = new Account(name, email);
             System.out.println(user1.toString());
         }
-
     }
 }
